93ee9cb94c0ad4cf575b6c8a7bbef205a5f15e9a
Faryan Rezagholi <faryan.rezagholi@siedle.de>
2019-02-02 21:31:38 +0100

    updated footer

--- include/html/footer.php
+++ include/html/footer.php
@@ -14,7 +14,7 @@ if ($pPageIsPublic && (preg_match('/(log(in|out)|user_password)\.php/',$_SERVER[
         echo '<p class="tznError"><b>'.$GLOBALS['langMenu']['warning'].':</b> '.$GLOBALS['langMenu']['warning_install'].'</p>';
     }
     ?>
-        <a href="http://www.taskfreak.com">TaskFreak! multi user</a> v<?php echo FRK_VERSION; ?> - Released on <?php echo FRK_VERSION_RELEASE; ?> under GNU General Public License
+        <a href="https://github.com/farosch/TaskFreak">TaskFreak! Multi User</a> v<?php echo FRK_VERSION; ?> - Released on <?php echo FRK_VERSION_RELEASE; ?> under GNU General Public License v3
     </div>
 </div>
 <?php
